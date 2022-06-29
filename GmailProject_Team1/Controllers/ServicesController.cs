690c9228cf191323fd88f4ee8e632c2500b8e211
Sagar Sankpal <sagarsankpal2255@gmail.com>
2022-06-29 13:07:41 +0530

    this add for services

---
+++ GmailProject_Team1/Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GmailProject_Team1.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult ShowAll()
+        {
+            return View();
+        }
+    }
+}
